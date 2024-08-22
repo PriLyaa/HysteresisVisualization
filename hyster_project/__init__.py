a0c72bf37e6f47af2c7d63b20f5f6a33d479003b
Ivan Prilepsky <prilepskii.ivan@gmail.com>
2024-08-22 13:17:22 +0300

    add correct shift by y and temp on legend

--- hyster_project/__init__.py
+++ hyster_project/__init__.py
@@ -1,4 +1,5 @@
 import os
+import re
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -11,6 +12,7 @@ files = [f for f in os.listdir(folder_path) if f.endswith('.dat')]
 
 # Создание фигуры для графика/ов
 plt.figure()
+plt.style.use('seaborn-v0_8-whitegrid')
 
 # Обрабатываем каждый файл по очереди
 for file_name in files:
@@ -19,20 +21,31 @@ for file_name in files:
     try:
         # Чтение данных из файла
         data = np.loadtxt(file_path)
-        
+
         # Преобразование А в mT
         data[:, 0] = -0.0546 * (data[:, 0] ** 3) + 0.0154 * (data[:, 0] ** 2) + 48.153 * data[:, 0]
-        
+
         # Нормализация второй колонки для приведения модулей минимального и максимального значений к одинаковому значению
         min_val = np.min(data[:, 1])
         max_val = np.max(data[:, 1])
-        #print(min_val, max_val)
-        max_mod = max(abs(min_val), abs(max_val))
-        data[:, 1] = np.interp(data[:, 1], [min_val, max_val], [-max_mod, max_mod])
-        #print(np.min(data[:, 1]), np.max(data[:, 1]))
+        print(min_val, max_val)
+        
+         # Определяем смещение, чтобы центрировать график
+        shift = (max_val + min_val) / 2
+        
+        # Смещение всех значений второй колонки для центрирования относительно 0
+        data[:, 1] -= shift
+        print(np.min(data[:, 1]), np.max(data[:, 1]))
+
+        # Извлечение части названия файла между "T=" и "C" с помощью регулярного выражения
+        match = re.search(r'T=(.*?)C', file_name)
+        if match:
+            plot_label = match.group(1)
+        else:
+            plot_label = file_name  # Если не удалось извлечь, использовать имя файла целиком
 
         # Добавление данных на общий график
-        plt.plot(data[:, 0], data[:, 1], label=file_name)
+        plt.plot(data[:, 0], data[:, 1], label=f'{plot_label} C')
 
     except Exception as e:
         print(f"Произошла ошибка при обработке файла {file_name}: {e}")
